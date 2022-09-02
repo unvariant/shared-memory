15da49c247ad1099415cd5b849c5aced9877fa79
unvariant <unvariant.winter@gmail.com>
2022-09-02 18:32:07 +0000

    clean up warnings

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,23 @@
 use std::{
-    io::{
-        stdout,
-        Write,
-    },
-    sync::atomic::{
-        AtomicUsize,
-        Ordering,
-    },
     ffi::CStr,
+    ptr,
 };
 
 use libc::{
-    O_RDONLY, O_RDWR, O_CREAT, O_EXCL, O_TRUNC,
-    S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH,
+    O_RDWR, O_CREAT,
+    S_IRUSR, S_IWUSR, S_IXUSR,
     c_void, c_char, c_int,
-    PROT_READ, PROT_WRITE, PROT_EXEC, PROT_NONE,
+    PROT_READ, PROT_WRITE,
     MAP_SHARED,
 };
 
 #[cfg(unix)]
-use std::os::unix::io::{
-    RawFd,
-    FromRawFd,
-};
+use std::os::unix::io::RawFd;
 
 extern "C" {
+    #[allow(dead_code)]
     fn atomic_load64 (addr: * mut c_void) -> u64;
+    #[allow(dead_code)]
     fn atomic_store64 (addr: * mut c_void, val: u64) -> u64;
 
     fn shm_open (path: * const c_char, flags: c_int, mode: c_int) -> RawFd;
@@ -35,13 +27,13 @@ extern "C" {
 }
 
 fn main () {
-    let fd: RawFd = unsafe { shm_open ("/test\0".as_ptr() as * const _, O_CREAT | O_RDWR, (S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH) as c_int) };
+    let fd: RawFd = unsafe { shm_open ("/test\0".as_ptr() as * const _, O_CREAT | O_RDWR, (S_IRUSR | S_IWUSR | S_IXUSR) as c_int) };
     println! ("fd: {fd}");
 
     let rs = unsafe { ftruncate (fd, 2048) };
     println! ("ftruncate: {rs}");
 
-    let buf: * mut i8 = unsafe { mmap (0 as * mut _, 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0) } as * mut _;
+    let buf: * mut i8 = unsafe { mmap (ptr::null_mut(), 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0) } as * mut _;
     println! ("buffer address: {:?}", buf);
 
     let mut n = 0;
