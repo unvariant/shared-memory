3995bdb38604d1f38a0a544672ccff54975f75b8
unvariant <unvariant.winter@gmail.com>
2022-09-02 18:33:38 +0000

    cargo fmt

--- build.rs
+++ build.rs
@@ -1,24 +1,31 @@
-fn main () {
-    #[cfg(target_arch="x86_64")] {
-        #[cfg(target_os="linux")] {
+fn main() {
+    #[cfg(target_arch = "x86_64")]
+    {
+        #[cfg(target_os = "linux")]
+        {
             println!("cargo:rustc-link-search=lib/x86_64-linux");
         }
     }
 
-    #[cfg(target_arch="arm")] {
-        #[cfg(target_os="linux")] {
-            println! ("cargo:rustc-link-search=lib/arm-linux");
+    #[cfg(target_arch = "arm")]
+    {
+        #[cfg(target_os = "linux")]
+        {
+            println!("cargo:rustc-link-search=lib/arm-linux");
         }
     }
 
-    #[cfg(target_arch="aarch64")] {
-        #[cfg(target_os="macos")] {
-            println! ("cargo:rustc-link-search=lib/aarch64-macos");
+    #[cfg(target_arch = "aarch64")]
+    {
+        #[cfg(target_os = "macos")]
+        {
+            println!("cargo:rustc-link-search=lib/aarch64-macos");
         }
-        #[cfg(target_os="linux")] {
-            println! ("cargo:rustc-link-search=lib/aarch64-linux");
+        #[cfg(target_os = "linux")]
+        {
+            println!("cargo:rustc-link-search=lib/aarch64-linux");
         }
     }
 
-    println! ("cargo:rustc-flags=-latomic");
+    println!("cargo:rustc-flags=-latomic");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use std::{
-    ffi::CStr,
-    ptr,
-};
+use std::{ffi::CStr, ptr};
 
 use libc::{
-    O_RDWR, O_CREAT,
-    S_IRUSR, S_IWUSR, S_IXUSR,
-    c_void, c_char, c_int,
-    PROT_READ, PROT_WRITE,
-    MAP_SHARED,
+    c_char, c_int, c_void, MAP_SHARED, O_CREAT, O_RDWR, PROT_READ, PROT_WRITE, S_IRUSR, S_IWUSR,
+    S_IXUSR,
 };
 
 #[cfg(unix)]
@@ -16,25 +10,47 @@ use std::os::unix::io::RawFd;
 
 extern "C" {
     #[allow(dead_code)]
-    fn atomic_load64 (addr: * mut c_void) -> u64;
+    fn atomic_load64(addr: *mut c_void) -> u64;
     #[allow(dead_code)]
-    fn atomic_store64 (addr: * mut c_void, val: u64) -> u64;
+    fn atomic_store64(addr: *mut c_void, val: u64) -> u64;
 
-    fn shm_open (path: * const c_char, flags: c_int, mode: c_int) -> RawFd;
-    fn shm_unlink (path: * const c_char) -> c_int;
-    fn ftruncate (fd: RawFd, len: usize) -> c_int;
-    fn mmap (addr: * mut c_void, len: usize, prot: c_int, flags: c_int, fd: RawFd, offset: usize) -> * mut c_void;
+    fn shm_open(path: *const c_char, flags: c_int, mode: c_int) -> RawFd;
+    fn shm_unlink(path: *const c_char) -> c_int;
+    fn ftruncate(fd: RawFd, len: usize) -> c_int;
+    fn mmap(
+        addr: *mut c_void,
+        len: usize,
+        prot: c_int,
+        flags: c_int,
+        fd: RawFd,
+        offset: usize,
+    ) -> *mut c_void;
 }
 
-fn main () {
-    let fd: RawFd = unsafe { shm_open ("/test\0".as_ptr() as * const _, O_CREAT | O_RDWR, (S_IRUSR | S_IWUSR | S_IXUSR) as c_int) };
-    println! ("fd: {fd}");
+fn main() {
+    let fd: RawFd = unsafe {
+        shm_open(
+            "/test\0".as_ptr() as *const _,
+            O_CREAT | O_RDWR,
+            (S_IRUSR | S_IWUSR | S_IXUSR) as c_int,
+        )
+    };
+    println!("fd: {fd}");
 
-    let rs = unsafe { ftruncate (fd, 2048) };
-    println! ("ftruncate: {rs}");
+    let rs = unsafe { ftruncate(fd, 2048) };
+    println!("ftruncate: {rs}");
 
-    let buf: * mut i8 = unsafe { mmap (ptr::null_mut(), 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0) } as * mut _;
-    println! ("buffer address: {:?}", buf);
+    let buf: *mut i8 = unsafe {
+        mmap(
+            ptr::null_mut(),
+            1024,
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED,
+            fd,
+            0,
+        )
+    } as *mut _;
+    println!("buffer address: {:?}", buf);
 
     let mut n = 0;
     while n == 0 {
@@ -42,8 +58,8 @@ fn main () {
     }
 
     let msg = unsafe { CStr::from_ptr(buf.add(1)) };
-    println! ("{:?}", msg);
+    println!("{:?}", msg);
 
-    let rs = unsafe { shm_unlink ("/test\0".as_ptr() as * const _) };
-    println! ("shm_unlink: {rs}");
+    let rs = unsafe { shm_unlink("/test\0".as_ptr() as *const _) };
+    println!("shm_unlink: {rs}");
 }
