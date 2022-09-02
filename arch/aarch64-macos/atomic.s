584945630eec506afe91c8f58879792a914462a5
ninefox <unvariant.winter@gmail.com>
2022-09-02 16:40:04 -0700

    add aarch64-macos static library

--- arch/aarch64-macos/atomic.s
+++ arch/aarch64-macos/atomic.s
@@ -1,11 +1,8 @@
     .global _atomic_load64
     .global _atomic_store64
 
-    .code 64
-    .section .text
 
-
-    .func _atomic_load64
+    .align 4
 
 _atomic_load64:
     mov     x9,      x0
@@ -14,16 +11,12 @@ _atomic_load64:
     cbnz    w10,     1b
     ret
 
-    .endfunc
 
-
-    .func _atomic_store64
-
+    .align 4
+    
 _atomic_store64:
     mov     x9,      x0
 1:  ldxr    x0,      [x9]
     stxr    w10,     x1,      [x9]
     cbnz    w10,     1b
     ret
-
-    .endfunc
